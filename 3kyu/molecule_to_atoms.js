c407db572efeb446343b9ec1464af215e1441aa2
stackcats <463642759@qq.com>
2016-09-05 00:48:47 +0800

    molecule_to_atoms.js

--- 3kyu/molecule_to_atoms.js
+++ 3kyu/molecule_to_atoms.js
@@ -1,9 +1,51 @@
-function parseMolecule(formula) {
-  // do your science here
-  return parse(formula, 1);
+function parse(arr) {
+  const ans = {};
+  for (let i = 0; i < arr.length; i++) {
+    const obj = {};
+    const ch = arr[i];
+    if (ch === '(') {
+      let flag = 1;
+      for (let j = i + 1; j < arr.length; j++) {
+        if (arr[j] === '(') {
+          flag++;
+        } else if (arr[j] === ')') {
+          flag--;
+        }
+        if (flag === 0) {
+          const tmp = arr.slice(i + 1, j);
+          const res = parse(tmp);
+          i = j;
+          Object.keys(res).forEach(r => {
+            obj[r] = res[r];
+          });
+          break;
+        }
+      }
+    } else {
+      obj[ch] = 1;
+    }
+    if (!isNaN(Number(arr[i + 1]))) {
+      Object.keys(obj).forEach(o => {
+        obj[o] *= Number(arr[i + 1]);
+      });
+      i++;
+    }
+
+    Object.keys(obj).forEach(o => {
+      if (ans[o] !== undefined) {
+        ans[o] += obj[o];
+      } else {
+        ans[o] = obj[o];
+      }
+    });
+  }
+  return ans;
 }
 
-function parse(formula, base) {
-  let dt = {};
-  for()
-    }
+function parseMolecule(formula) {
+  // do your science here
+  formula = formula.replace(/[\[{]/g, '(').replace(/[\]}]/g, ')');
+  const arr = formula.split(/([A-Z][a-z]?)|(\d+)|(\()/g)
+        .filter(s => s && !s.match(/^\s*$/));
+  return parse(arr);
+}
