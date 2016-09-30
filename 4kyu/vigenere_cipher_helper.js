5040748c9a0344a94cb5c164ec6b58adfca3c76e
stackcats <463642759@qq.com>
2016-09-30 14:42:21 +0800

    vigenere cipher helper

---
+++ 4kyu/vigenere_cipher_helper.js
@@ -0,0 +1,29 @@
+function VigenèreCipher(key, abc) {
+  this.encode = function encode(str) {
+    let ans = '';
+    for (let i = 0; i < str.length; i++) {
+      const c = str[i];
+      const p = key[i % key.length];
+      if (abc.indexOf(c) === -1) {
+        ans += c;
+      } else {
+        ans += abc[(abc.indexOf(p) + abc.indexOf(c)) % abc.length];
+      }
+    }
+    return ans;
+  };
+  this.decode = function decode(str) {
+    let ans = '';
+    for (let i = 0; i < str.length; i++) {
+      const c = str[i];
+
+      const p = key[i % key.length];
+      if (abc.indexOf(c) === -1 || abc.indexOf(p) === -1) {
+        ans += c;
+      } else {
+        ans += abc[(abc.indexOf(c) - abc.indexOf(p)) % abc.length];
+      }
+    }
+    return ans;
+  };
+}
