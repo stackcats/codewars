8ecbc3970c7c1d7f121ac9434edfe979c0075875
stackcats <stackcats.hk@gmail.com>
2026-08-29 17:17:21 +0800

    FlickSwitch.hs

---
+++ 8kyu/FlickSwitch.hs
@@ -0,0 +1,8 @@
+module FlickSwitch (flickSwitch) where
+
+flickSwitch :: [String] -> [Bool]
+flickSwitch =
+        reverse . fst . foldl f ([], True)
+    where
+        f (acc, b) "flick" = (not b : acc, not b)
+        f (acc, b) _ = (b : acc, b)
